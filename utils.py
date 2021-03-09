a1af6eaf6376dd16b5489fe8af800ad3a1b0ccb2
Yuval Simon <yuvalsimon2@gmail.com>
2021-03-09 20:01:24 +0200

    trim propensity

--- utils.py
+++ utils.py
@@ -23,5 +23,32 @@ def propensity_func(df, t_col='is_weekend', y_col='shares', method='log', **kwar
         propensity_model = RandomForestClassifier(random_state=random_state, **kwargs)
     elif(method == 'boosting'):
         propensity_model = GradientBoostingClassifier(random_state=random_state, **kwargs)
+    else:
+        raise NotImplementedError(method)
     propensity_model.fit(x, t)
     return lambda x: propensity_model.predict_proba(x)[:,1]
+
+def trim_common_support(data, label_name):
+    """ Removes observations that fall outside the common support of the propensity score 
+        distribution from the data.
+    
+    Arguments:
+    ----------
+        data:        DataFrame with the propensity scores for each observation.
+        label_name:  Column name that contains the labels (treatment/control) for each observation.
+    
+    """
+    min_propensity = (data.groupby(label_name) \
+                         .propensity.agg(min_propensity='min')).reset_index()['min_propensity']
+    max_propensity = (data.groupby(label_name) \
+                         .propensity.agg(max_propensity='max')).reset_index()['max_propensity']
+
+    # Compute boundaries of common support between the two propensity score distributions
+    min_common_support = np.max(min_propensity)
+    max_common_support = np.min(max_propensity)
+
+    common_support = (data.propensity >= min_common_support) & (data.propensity <= max_common_support)
+    control = (data[label_name] == 0)
+    treated = (data[label_name] == 1)
+    
+    return data[common_support]
