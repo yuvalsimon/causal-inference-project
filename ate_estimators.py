a52969ac77e1ee62e33bbb18904fa8afff12b61c
Yuval Simon <yuvalsimon2@gmail.com>
2021-03-16 02:25:07 +0200

    report

--- ate_estimators.py
+++ ate_estimators.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
 
 def ipw_ate(df, propensity_scores, t_col='is_weekend', y_col='shares'):
     return (((df[t_col] == 1).astype('float64') * df[y_col]) / propensity_scores \
@@ -26,12 +27,12 @@ def matching_ate(df, t_col='is_weekend', y_col='shares'):
     return ((y1.values - y0.iloc[d.argmin(axis=1)].values).sum() \
         + (y1.iloc[d.T.argmin(axis=1)].values - y0.values).sum()) /  len(df)
 
-def s_learner_ate(df, t_col='is_weekend', y_col='shares'):
+def s_learner_ate(df, t_col='is_weekend', y_col='shares', **kwargs):
     xt = df.drop([y_col], axis=1)
     x1 = xt[xt[t_col]==1]
     y = df[y_col]
     
-    f_x_t = LinearRegression().fit(xt, y)
+    f_x_t = GradientBoostingRegressor(**kwargs).fit(xt, y)
     
     xt_0 = xt.copy()
     xt_0[t_col] = 0
@@ -40,32 +41,35 @@ def s_learner_ate(df, t_col='is_weekend', y_col='shares'):
     
     return (f_x_t.predict(xt_1) - f_x_t.predict(xt_0)).mean()
 
-def t_learner_ate(df, t_col='is_weekend', y_col='shares'):
+def t_learner_ate(df, t_col='is_weekend', y_col='shares', **kwargs):
     x = df.drop([t_col,y_col], axis=1)
     x0 = df[df[t_col]==0].drop([t_col,y_col], axis=1)
     x1 = df[df[t_col]==1].drop([t_col,y_col], axis=1)
     y0 = df[df[t_col]==0][y_col]
     y1 = df[df[t_col]==1][y_col]
     
-    f_x_0 = LinearRegression().fit(x0, y0)
-    f_x_1 = LinearRegression().fit(x1, y1)
+    f_x_0 = GradientBoostingRegressor(**kwargs).fit(x0, y0)
+    f_x_1 = GradientBoostingRegressor(**kwargs).fit(x1, y1)
+    
+    print(f_x_0.score(x0, y0))
+    print(f_x_1.score(x1, y1))
     
     return (f_x_1.predict(x) - f_x_0.predict(x)).mean()
 
-def x_learner_ate(df, propensity_scores, t_col='is_weekend', y_col='shares'):
+def x_learner_ate(df, propensity_scores, t_col='is_weekend', y_col='shares', **kwargs):
     x = df.drop([t_col,y_col], axis=1)
     x0 = df[df[t_col]==0].drop([t_col,y_col],axis=1)
     x1 = df[df[t_col]==1].drop([t_col,y_col],axis=1)
     y0 = df[df[t_col]==0][y_col]
     y1 = df[df[t_col]==1][y_col]
 
-    f_x_1 = LinearRegression().fit(x1, y1)
-    f_x_0 = LinearRegression().fit(x0, y0)
+    f_x_1 = GradientBoostingRegressor(**kwargs).fit(x1, y1)
+    f_x_0 = GradientBoostingRegressor(**kwargs).fit(x0, y0)
     
     ite_1 = y1 - f_x_0.predict(x1)
     ite_0 = f_x_1.predict(x0) - y0
     
-    f_ite_1 = RandomForestRegressor().fit(x1, ite_1)
-    f_ite_0 = RandomForestRegressor().fit(x0, ite_0)
+    f_ite_1 = GradientBoostingRegressor(**kwargs).fit(x1, ite_1)
+    f_ite_0 = GradientBoostingRegressor(**kwargs).fit(x0, ite_0)
     
     return (propensity_scores * f_ite_0.predict(x) + (1 - propensity_scores) * f_ite_1.predict(x)).mean()
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
+from collections import namedtuple
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.feature_selection import RFE
 import holidays
 
 random_state = 101
@@ -19,45 +21,25 @@ def factorize(df):
         for s in groups[np.dtype('O')]:
             df[s] = df[s].factorize()[0]
 
+prop_func = namedtuple("prop_func", "func model")
+
 def propensity_func(df, t_col='is_weekend', y_col='shares', method='log', **kwargs):
     x = df.drop([t_col, y_col],axis=1)
     t = df[t_col]
 
     if(method == 'log'):
         propensity_model = LogisticRegression(random_state=random_state, **kwargs)
+#         propensity_model = RFE(estimator=propensity_model)
     elif(method == 'random_forest'):
         propensity_model = RandomForestClassifier(random_state=random_state, **kwargs)
+#         propensity_model = RFE(estimator=propensity_model)
     elif(method == 'boosting'):
         propensity_model = GradientBoostingClassifier(random_state=random_state, **kwargs)
+#         propensity_model = RFE(estimator=propensity_model)
     else:
         raise NotImplementedError(method)
     propensity_model.fit(x, t)
-    return lambda x: propensity_model.predict_proba(x)[:,1]
-
-# def trim_common_support(data, label_name):
-#     """ Removes observations that fall outside the common support of the propensity score 
-#         distribution from the data.
-    
-#     Arguments:
-#     ----------
-#         data:        DataFrame with the propensity scores for each observation.
-#         label_name:  Column name that contains the labels (treatment/control) for each observation.
-    
-#     """
-#     min_propensity = (data.groupby(label_name) \
-#                          .propensity.agg(min_propensity='min')).reset_index()['min_propensity']
-#     max_propensity = (data.groupby(label_name) \
-#                          .propensity.agg(max_propensity='max')).reset_index()['max_propensity']
-
-#     # Compute boundaries of common support between the two propensity score distributions
-#     min_common_support = np.max(min_propensity)
-#     max_common_support = np.min(max_propensity)
-
-#     common_support = (data.propensity >= min_common_support) & (data.propensity <= max_common_support)
-#     control = (data[label_name] == 0)
-#     treated = (data[label_name] == 1)
-    
-#     return data[common_support]
+    return prop_func(lambda x: propensity_model.predict_proba(x)[:,1], propensity_model)
 
 def propensity_hist_values(df, t, t_label='is_weekend'):
     hist = (df[df[t_label] == t]['propensity'] * 100).apply(np.round).astype('int').value_counts()
